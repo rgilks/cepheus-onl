3f00d9b1260afe82526721e14359ee6546bd2fc6
rgilks <rob.gilks@gmail.com>
2025-06-15 15:53:14 +0100

    feat(auth): migrate to next-auth v5 to resolve cloudflare issues

---
+++ lib/auth.ts
@@ -0,0 +1,37 @@
+import NextAuth from 'next-auth';
+import Discord from 'next-auth/providers/discord';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import { getDb } from '@/lib/db';
+import * as schema from '@/lib/db/schema';
+import { nanoid } from 'nanoid';
+import { Adapter } from 'next-auth/adapters';
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth(() => {
+  const db = getDb();
+  return {
+    adapter: {
+      ...(DrizzleAdapter(db, {
+        usersTable: schema.users,
+        accountsTable: schema.accounts,
+        sessionsTable: schema.sessions,
+        verificationTokensTable: schema.verificationTokens,
+      }) as Adapter),
+      createUser: user => {
+        return db
+          .insert(schema.users)
+          .values({ ...user, id: nanoid() })
+          .returning()
+          .then(res => res[0]);
+      },
+    },
+    providers: [
+      Discord({
+        clientId: process.env.DISCORD_CLIENT_ID,
+        clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      }),
+    ],
+  };
+});
